958c01a5e9e955da81ac672ac8b805acb55f1f52
AdityaGawde97 <57165852+AdityaGawde97@users.noreply.github.com>
2022-03-15 13:15:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
